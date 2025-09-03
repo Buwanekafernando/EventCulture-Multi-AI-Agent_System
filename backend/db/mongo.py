f7bdde14ed9ccc0af36b45e73002a0d7b6c2fcf0
Buwaneka Deshan Fernando <133486123+Buwanekafernando@users.noreply.github.com>
2025-09-03 06:20:22 +0530

    Update requirementstext

---
+++ backend/db/mongo.py
@@ -0,0 +1,7 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+client = AsyncIOMotorClient(os.getenv("MONGO_URI"))
+db = client["event_finder"]
