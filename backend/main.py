303dace0b63201df110c7058dc8bc692afe2893e
Buwaneka Deshan Fernando <133486123+Buwanekafernando@users.noreply.github.com>
2025-09-13 22:40:24 +0530

    conversion setup

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
-from agents.orchestrator import router as orchestrator_router
-from auth.google_oauth import router as auth_router
+from router import event
+from auth.google_auth import router as auth_router
+from db.database import Base, engine
 
 app = FastAPI()
 
 app.include_router(orchestrator_router, prefix="/api")
 app.include_router(auth_router, prefix="/auth")
+
+#database initialize
+Base.metadata.create_all(bind=engine)
+
+
+
+app = FastAPI()
+app.include_router(event.router)
