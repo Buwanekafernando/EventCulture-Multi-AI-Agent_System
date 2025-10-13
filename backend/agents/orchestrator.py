0fd1de905870f5352dd34af8cd20072ce57137de
Buwaneka Deshan Fernando <133486123+Buwanekafernando@users.noreply.github.com>
2025-10-13 14:52:11 +0530

    update

--- backend/agents/orchestrator.py
+++ backend/agents/orchestrator.py
@@ -1,16 +1,16 @@
 from fastapi import APIRouter
 from agents.event_collector import router as collector_router
 from agents.nlp_agent import router as nlp_router
+from router.rec_agent import router as recommender_router
+from agents.location_agent import router as location_router
 
 router = APIRouter()
 
 #event collector agent
 router.include_router(collector_router, prefix="/collector")
 
-#<<<<<<< agent-praneepa
 
-router.include_router(recommender_router, prefix="/recommend")
-#=======
-#nlp agent
-router.include_router(nlp_router, prefix="/nlp")
-#>>>>>>> main
+
+# Location Agent
+router.include_router(location_router, prefix="/location")
+
